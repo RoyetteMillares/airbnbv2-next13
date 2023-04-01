d14c4967b854ec722d691e3abe600d26dd39c93f
royetteM <royettezxc@proton.me>
2023-04-01 18:36:59 +0800

    add search menu

---
+++ app/components/UserMenu.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const UserMenu = () => {
+    return (
+      <div className="relative">
+            <div className="flex flex-row items-center gap-3">
+                <div onClick={() => {} }
+                className='
+                     hiddend md:block text-sm font-semibold py-3 px-4 rounded-full transition hover:bg-neutral-100
+                    cursor-pointer
+                '>
+                    
+               </div>
+            </div>
+      </div>
+    );
+}
+
+export default UserMenu
